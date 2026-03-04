83d562f346b3c6cb0c4880f8e2b47a7b3db0714f
Othon Maranhão <othonb@yahoo.com>
2026-03-04 09:50:47 -0300

    Template com bootstrap para index e exibição de Question

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,17 @@
+from django.template import loader
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.utils import timezone
+
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def create_poll(request):
     return HttpResponse("You're at the polls creator.")
@@ -20,7 +26,11 @@ def delete_poll(request):
     return HttpResponse("You're at the polls delete.")
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
